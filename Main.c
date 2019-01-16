d977a69c5f59908b898c31557670734fb66f160f
Jackson1412able <46261099+Jackson1412able@users.noreply.github.com>
2019-01-16 21:57:48 +0800

    Main.c

--- Main.c
+++ Main.c
@@ -3,7 +3,7 @@
     , resistance, inductance and capacitance measurements. The system then generates 200 pseudo-random
     frequency inputs and calculate impedance and voltage based on the generated frequencies. The voltages
     will then be plotted in a web page and the maximum and minimum voltages are indicated in the plot.
-    The C program was written to generate the design of the web page using HTML, CSS and Javascript.
+    The C program was written to generate the design of the web page using HTML and CSS.
 */
 
 #include <stdio.h>
